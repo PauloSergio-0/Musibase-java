46e8628462837e1f26625c065f40a9138a86b5f3
Paulo Sérgio <paulosergiosrs08@gmail.com>
2025-06-24 13:53:25 -0300

    inital commit

---
+++ src/main/java/com/Musibase/Musibase/MusibaseApplication.java
@@ -0,0 +1,13 @@
+package com.Musibase.Musibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Musibase/Musibase/MusibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Musibase.Musibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
